515f014cc657c4aead0456a5f3a2aab313158ec6
Geoffrey Jones <jonesg5@rpi.edu>
2014-07-21 11:38:01 -0400

    PortlandPeople: Added additional sleep outcomes

--- PortlandPeople/batchsleep.m
+++ PortlandPeople/batchsleep.m
@@ -27,9 +27,16 @@ BedLog = importbedlog(bedLogPath);
 Output = struct(...
     'subject'               , {[]} ,...
     'night'                 , {[]} ,...
-    'actualSleepTimeMins'   , {[]} ,...
-    'sleepEfficiency'       , {[]} ,...
-    'sleepOnsetLatencyMins' , {[]} );
+    'actualSleepTimeMins',    {[]},...
+    'actualSleepPercent',     {[]},...
+    'actualWakeTimeMins',     {[]},...
+    'actualWakePercent',      {[]},...
+    'sleepEfficiency',        {[]},...
+    'sleepOnsetLatencyMins',  {[]},...
+    'sleepBouts',             {[]},...
+    'wakeBouts',              {[]},...
+    'meanSleepBoutTimeMins',  {[]},...
+    'meanWakeBoutTimeMins',   {[]});
 
 ii = 1;
 for i1 = 1:nCdf
@@ -69,9 +76,17 @@ for i1 = 1:nCdf
         % Assign results to output struct
         Output(ii,1).subject               = subject;
         Output(ii,1).night                 = i2;
-        Output(ii,1).actualSleepTimeMins   = Sleep.actualSleepTime;
-        Output(ii,1).sleepEfficiency       = Sleep.sleepEfficiency;
-        Output(ii,1).sleepOnsetLatencyMins = Sleep.sleepLatency;
+        
+        Output(ii,1).actualSleepTimeMins    = Sleep.actualSleepTime;
+        Output(ii,1).actualSleepPercent     = Sleep.actualSleepPercent;
+        Output(ii,1).actualWakeTimeMins     = Sleep.actualWakeTime;
+        Output(ii,1).actualWakePercent      = Sleep.actualWakePercent;
+        Output(ii,1).sleepEfficiency        = Sleep.sleepEfficiency;
+        Output(ii,1).sleepOnsetLatencyMins  = Sleep.sleepLatency;
+        Output(ii,1).sleepBouts             = Sleep.sleepBouts;
+        Output(ii,1).wakeBouts              = Sleep.wakeBouts;
+        Output(ii,1).meanSleepBoutTimeMins	= Sleep.meanSleepBoutTime;
+        Output(ii,1).meanWakeBoutTimeMins	= Sleep.meanWakeBoutTime;
         
         ii = ii + 1; % increment independent counter
     end
@@ -80,13 +95,56 @@ end
 
 % Prepare results for output
 OutputDataset = struct2dataset(Output);
+idxEmpty = cellfun(@isempty,OutputDataset.actualSleepTimeMins);
+OutputDataset(idxEmpty,:) = [];
 outputCell = dataset2cell(OutputDataset);
 varNameArray = outputCell(1,:);
 prettyVarNameArray = lower(regexprep(varNameArray,'([^A-Z])([A-Z0-9])','$1 $2'));
 outputCell(1,:) = prettyVarNameArray;
+
+% Average results
+AveragedOutput = struct(...
+    'subject'               , {[]} ,...
+    'nightsAveraged'        , {[]} ,...
+    'actualSleepTimeMins',    {[]},...
+    'actualSleepPercent',     {[]},...
+    'actualWakeTimeMins',     {[]},...
+    'actualWakePercent',      {[]},...
+    'sleepEfficiency',        {[]},...
+    'sleepOnsetLatencyMins',  {[]},...
+    'sleepBouts',             {[]},...
+    'wakeBouts',              {[]},...
+    'meanSleepBoutTimeMins',  {[]},...
+    'meanWakeBoutTimeMins',   {[]});
+unqSubjectArray = unique(OutputDataset.subject);
+
+varnames = fieldnames(Output);
+idxSubjectVar = strcmp('subject',varnames);
+idxNightVar = strcmp('night',varnames);
+idxVarRemove = idxSubjectVar | idxNightVar;
+varnames(idxVarRemove) = [];
+
+for j1 = 1:numel(unqSubjectArray)
+    AveragedOutput(j1,1).subject = unqSubjectArray(j1);
+    idxSubject = OutputDataset.subject == unqSubjectArray(j1);
+    AveragedOutput(j1,1).nightsAveraged = numel(OutputDataset.night(idxSubject));
+    
+    for j2 = 1:numel(varnames)
+        AveragedOutput(j1,1).(varnames{j2}) = mean(cell2mat(OutputDataset.(varnames{j2})(idxSubject)));
+    end
+end
+
+% Prepare results for output
+AveragedOutputDataset = struct2dataset(AveragedOutput);
+averagedOutputCell = dataset2cell(AveragedOutputDataset);
+varNameArray2 = averagedOutputCell(1,:);
+prettyVarNameArray2 = lower(regexprep(varNameArray2,'([^A-Z])([A-Z0-9])','$1 $2'));
+averagedOutputCell(1,:) = prettyVarNameArray2;
+
 % Save results to an MS Excel file
 xlswrite([resultsPath,'.xlsx'],outputCell);
-% Save results to a Matlab file
-save([resultsPath,'.mat'],'Output');
+xlswrite([resultsPath,'_averaged.xlsx'],averagedOutputCell);
+% % Save results to a Matlab file
+% save([resultsPath,'.mat'],'Output');
 
 end
