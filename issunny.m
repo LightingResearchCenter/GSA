84d34211c7f2a33f1dc82f36db22568a27bdd5c5
Geoffrey Jones <jonesg5@rpi.edu>
2014-07-25 11:00:08 -0400

    Copied issunny

---
+++ issunny.m
@@ -0,0 +1,17 @@
+function sunnyDayLogical = issunny(timeArray,sunnyDayArray)
+%ISSUNNY Summary of this function goes here
+%   Detailed explanation goes here
+
+% Convert time to days
+dayArray = floor(timeArray);
+
+% Preallocate output array
+sunnyDayLogical = false(size(dayArray));
+
+% Find matching dates (true = match)
+for i1 = 1:numel(sunnyDayArray)
+    sunnyDayLogical = sunnyDayLogical | (dayArray == sunnyDayArray(i1));
+end
+
+end
+
