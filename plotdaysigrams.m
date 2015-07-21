b080cc60a1db46ba8849b6f835beb733337ae6a6
Geoffrey Jones <jonesg5@rpi.edu>
2015-07-21 15:52:37 -0400

    Update paths and locations

--- plotdaysigrams.m
+++ plotdaysigrams.m
@@ -13,30 +13,40 @@ initializedependencies;
 Paths = initializepaths(plainLocation,plainSession);
 [cdfNameArray,cdfPathArray] = searchdir(Paths.editedData,'cdf');
 
+mkdir(Paths.plots,'daysigrams-cs');
+mkdir(Paths.plots,'daysigrams-lux');
+
+dirCs = fullfile(Paths.plots,'daysigrams-cs');
+dirLux = fullfile(Paths.plots,'daysigrams-lux');
+
 nFiles = numel(cdfPathArray);
 for i1 = 1:nFiles
     % Import data
-    Data = ProcessCDF(cdfPathArray{i1});
-    locationID = Data.GlobalAttributes.subjectID{1};
-    logicalArray = logical(Data.Variables.logicalArray);
-    complianceArray = logical(Data.Variables.complianceArray(logicalArray));
-    timeArray = Data.Variables.time(logicalArray);
-    activityArray = zeros(size(timeArray));
-    csArray = Data.Variables.CS(logicalArray);
-    illuminanceArray = Data.Variables.illuminance(logicalArray);
+    cdfData = daysimeter12.readcdf(cdfPathArray{i1});
+    [absTime,relTime,epoch,light,activity,masks,subjectID,deviceSN] = daysimeter12.convertcdf(cdfData);
+    
+%     absTime.localDateNum = absTime.localDateNum(masks.observation);
+%     light.cs = light.cs(masks.observation);
+%     light.illuminance = light.illuminance(masks.observation);
+%     activity = activity(masks.observation);
+%     masks.bed = masks.bed(masks.observation);
+%     masks.compliance = masks.compliance(masks.observation);
+%     masks.observation = masks.observation(masks.observation);
+    
+    locationID = subjectID;
+    activity = zeros(size(activity));
     
     % Daysigram
     sheetTitle = ['GSA - ',displayLocation,' - ',displaySession,' - ',locationID];
+    sheetTitle = regexprep(sheetTitle,'_','\\_');
     
     daysigramFileID = ['location',locationID,'_cs'];
-    generatedaysigram(sheetTitle,timeArray(complianceArray),...
-        activityArray(complianceArray),csArray(complianceArray),...
-        'cs',[0,1],14,Paths.plots,daysigramFileID);
+    
+    reports.daysigram.daysigram(1,sheetTitle,absTime.localDateNum,masks,activity,light.cs,'cs',[0,1],14,dirCs,daysigramFileID);
     
     daysigramFileID = ['location',locationID,'_lux'];
-    generatedaysigram(sheetTitle,timeArray(complianceArray),...
-        activityArray(complianceArray),illuminanceArray(complianceArray),...
-        'lux',[0.01,10^5],14,Paths.plots,daysigramFileID);
+    
+    reports.daysigram.daysigram(2,sheetTitle,absTime.localDateNum,masks,activity,light.illuminance,'lux',[0.01,10^5],14,dirLux,daysigramFileID);
     
 end
 
